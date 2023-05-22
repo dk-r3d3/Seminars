3604890d60e37187c753e03c8afd72ffb66d940c
dk_r3 <dk.r3d3@gmail.com>
2023-05-22 22:16:36 +0700

    initial commit

---
+++ seminar_1/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.Write("max = " + a);
+}
+else if (b > a && b > c)
+{
+    Console.Write("max = " + b);   
+}
+else if (c > a && c > b)
+{
+    Console.Write("max = " + c);   
+}
