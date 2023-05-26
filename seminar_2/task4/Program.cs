d3d8dcf5ee89f5b2ef6dca1d86b10b70f7ead49d
dk_r3 <dk.r3d3@gmail.com>
2023-05-26 23:08:16 +0700

    homework_2

--- seminar_2/task4/Program.cs
+++ seminar_2/task4/Program.cs
@@ -4,7 +4,4 @@
 // 1 -> нет
 // 567,123 -> 57,123
 
-System.Console.Write("Введите любое число:  ");
-var num = Console.ReadLine();
-var[] array = num;
-System.Console.WriteLine(array);
+// НЕ РЕШЕНА
