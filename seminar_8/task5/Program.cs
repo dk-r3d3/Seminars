2767150c63e604f7fcfee0e8b1ebc6c9ab6003d8
dk_r3 <dk.r3d3@gmail.com>
2023-06-21 22:49:36 +0700

    task 5 for 4x4

--- seminar_8/task5/Program.cs
+++ seminar_8/task5/Program.cs
@@ -8,38 +8,68 @@
 
 // пока не решил
 
-// int[,] matrix = new int[,]
-// {
-//     {0, 0, 0, 0},
-//     {0, 0, 0, 0},
-//     {0, 0, 0, 0},
-//     {0, 0, 0, 0}
-// };
+int[,] matrix = new int[,]
+{
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0},
+    {0, 0, 0, 0}
+};
 
-// void PrintArray(int[,] collection)
-// {
-//     for (int i = 0; i < collection.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < collection.GetLength(1); j++)
-//             {
-//                 Console.Write($"{collection[i,j],1}  ");
-//             }
-//             System.Console.WriteLine();
-//         }
-// }
+void PrintArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+        {
+            for (int j = 0; j < collection.GetLength(1); j++)
+            {
+                Console.Write($"{collection[i,j],1}   ");
+            }
+            System.Console.WriteLine();
+        }
+}
 
-// void FillMatrix(int row, int col)
-// {
-//     if (matrix[row, col] == 0)
-//     {
-//         matrix[row, col] = 0;
-//         FillMatrix(row - 1, col);
-//         FillMatrix(row, col - 1);
-//         FillMatrix(row + 1, col);
-//         FillMatrix(row, col + 1);
-//     }
-// }
+void FillMatrix(int rows, int cols)
+{
+    int count = 1;
+    int countrow = 0;
+    int countcolumn = 0;
+    int row = matrix.GetLength(0);
+    int column = matrix.GetLength(1);
+    for (int i = 0; i < matrix.GetLength(1) - 1; i++) // строка 1
+    {
+        matrix[countrow, i] = count;
+        count++;
+    }
+    countrow++;
+    for (int j = 0; j < matrix.GetLength(0); j++) // столбец 4
+    {
+        matrix[j, (column - 1) - countcolumn] = count;
+        count++;
+    }
+    for (int k = matrix.GetLength(1) - 2; k > 0 ; k--) // строка 4
+    {
+        matrix[row - countrow, k] = count;
+        count++;
+    }
+    for (int n = matrix.GetLength(0) - 1; n > 0; n--) // столбец 1
+    {
+        matrix[n, countcolumn] = count;
+        count++;
+    }
+    for (int m = 1; m <= matrix.GetLength(1) - 2; m++) // строка 2
+    {
+        matrix[countrow, m] = count;
+        count++;
+    }
+    countrow++;
+    for (int p = matrix.GetLength(0) - 2; p > 0; p--) // строка 3
+    {
+        matrix[countrow, p] = count;
+        count++;
+    }
+}
 
-
-// FillMatrix(4, 4);
-// PrintArray(matrix);
+PrintArray(matrix);
+FillMatrix(0, 0);
+System.Console.WriteLine();
+PrintArray(matrix);
